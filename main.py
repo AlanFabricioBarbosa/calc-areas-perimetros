e93cebd12972afd225cd887afbb5ba5b1cfd49a2
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2025-03-22 20:28:06 -0300

    feat: Feito funções para calcular a área e o perímetro de um triângulo

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-def calc_quadrilateral_area(width, lenght):
-   area = width * lenght
+def calc_quadrilateral_area(width, length):
+   area = width * length
    return area
 
-print(calc_quadrilateral_area(5, 2))
+print(f"\nA área do quadrilátero é: {calc_quadrilateral_area(5, 2)}")
 
-def calc_quadrilateral_perimeter(width, lenght):
-   perimeter = 2 * (width + lenght)
+def calc_quadrilateral_perimeter(width, length):
+   perimeter = 2 * (width + length)
    return perimeter
 
-print(calc_quadrilateral_perimeter(5,6))
+print(f"\nO perímetro do quadrilátero é: {calc_quadrilateral_perimeter(5,6)}")
+
+def calc_triangle_area(base, height):
+   area = (base * height) / 2
+   return area
+
+print(f"\nA área do triângulo é: {calc_triangle_area(5, 2)}")
+
+def calc_triangle_perimeter(a, b, c):
+   perimeter = a + b + c
+   return perimeter
+
+print(f"\nO perímetro do triângulo é: {calc_triangle_perimeter(7,9,5)}")
