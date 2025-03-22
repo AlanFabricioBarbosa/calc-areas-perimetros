97a4e0760ea09d0987897d4ee08387f1c54e0fb8
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2025-03-22 20:02:01 -0300

    feat: Feito funções para calcular a área e o perímetro de um quadrilátero

---
+++ main.py
@@ -0,0 +1,11 @@
+def calc_quadrilateral_area(width, lenght):
+   area = width * lenght
+   return area
+
+print(calc_quadrilateral_area(5, 2))
+
+def calc_quadrilateral_perimeter(width, lenght):
+   perimeter = 2 * (width + lenght)
+   return perimeter
+
+print(calc_quadrilateral_perimeter(5,6))
